cf97ead6e0efc4bdf539dcb04d0b497406d63e02
Emily Carolina <128648356+Huoyiin@users.noreply.github.com>
2023-04-28 16:43:45 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe a distância percorrida pelo veículo (km): ");
+        double distancia = scanner.nextDouble();
+
+        System.out.println("Informe o total de combustível gasto (litros): ");
+        double combustivel = scanner.nextDouble();
+
+        double consumo = distancia/combustivel;
+
+        System.out.println("O consumo médio do veículo foi de: "+ consumo);
+    }
+}
