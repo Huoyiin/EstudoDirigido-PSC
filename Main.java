8afae29f7dde72bec4959753bf24981a2c39f6dc
Emily Carolina <128648356+Huoyiin@users.noreply.github.com>
2023-04-29 17:48:53 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o valor a ser depositado: ");
+        double deposito = scanner.nextDouble();
+        System.out.println("Escolha o rendimento desejado: ");
+        System.out.println("1 - Poupança 0,90% ao mês.");
+        System.out.println("2 - CDB em 1,1% ao mês. ");
+        System.out.println("3 - Previdência Privada 1,3% ao mês.");
+        int escolha = scanner.nextInt();
+
+        double rendimento = 0.0;
+
+        switch (escolha) {
+            case 1:
+                rendimento = deposito * 0.09;
+                break;
+            case 2:
+                rendimento = deposito * 0.11;
+                break;
+            case 3:
+                rendimento = deposito * 0.13;
+                break;
+            default:
+                System.out.println("Essa opção é inválida.");
+        }
+        double rendimentoTotal = deposito + rendimento;
+        System.out.println("Valor total após um mês: R$" + rendimentoTotal);
+        for (int i = 2; i <= 12; i++) {
+            rendimentoTotal += rendimentoTotal * deposito;
+        }
+        double valorTotal = rendimentoTotal - deposito;
+        System.out.println("Valor total após 12 meses: R$" + valorTotal);
+    }
+}
