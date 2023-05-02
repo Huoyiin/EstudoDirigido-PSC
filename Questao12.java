44d0af42bda565d635b269ba8faf0c59bd6c6702
Emily Carolina <128648356+Huoyiin@users.noreply.github.com>
2023-05-02 18:27:51 -0300

    Questao12.java

--- Questao12.java
+++ Questao12.java
@@ -9,21 +9,10 @@ public class Main {
         System.out.print("Digite o valor de z: ");
         int z = scanner.nextInt();
 
-        double resul1 = calcularPot(x, z);
-        System.out.println(x + " elevado a " + z + ": " + resul1);
-
-        double resul2 = calcularPot2(x, z);
+        double resul = calcularPot(x, z);
+        System.out.println(x + " elevado a " + z + ": " + resul);
     }
-
     public static double calcularPot(int x, int z) {
         return Math.pow(x, z);
     }
-
-    public static double calcularPot2(int x, int z) {
-        double resultado = 1;
-        for (int i = 0; i < z; i++) {
-            resultado *= x;
-        }
-        return resultado;
     }
-}
