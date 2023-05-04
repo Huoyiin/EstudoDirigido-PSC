431553727653b19bac73f23c09607085b8030d8f
Emily Carolina <128648356+Huoyiin@users.noreply.github.com>
2023-05-03 21:29:25 -0300

    Questao11.java

--- Questao11.java
+++ Questao11.java
@@ -1,3 +1,6 @@
+//Faça uma classe com um método que será usado em todo sistema que recebe 3 valores
+//inteiros passados por parâmetro e retorna-os ordenados em ordem crescente.
+
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
