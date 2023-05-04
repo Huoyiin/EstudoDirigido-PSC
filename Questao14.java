6467b851017d64a0bd6df960017c287b8c26c640
Emily Carolina <128648356+Huoyiin@users.noreply.github.com>
2023-05-03 21:30:31 -0300

    Questao14.java

--- Questao14.java
+++ Questao14.java
@@ -1,3 +1,6 @@
+//Implemente o programa em Java acima e execute o programa e veja a exceção
+//produzida, corrija o código com uma solução para resolver esta exceção no código.
+
 public class Main {
     public static void main(String[] args) {
         int[] vetor = new int[] { 2, 4, 6, 8, 10, 12 };
