e0db2f5442dcd7575fd5812a03e98d124a89929c
Emily Carolina <128648356+Huoyiin@users.noreply.github.com>
2023-05-03 21:24:42 -0300

    Questao1.java

--- Questao1.java
+++ Questao1.java
@@ -1,3 +1,5 @@
+//Escrever um programa para determinar o consumo médio de um automóvel sendo
+//fornecida a distância total percorrida pelo automóvel e o total de combustível gasto.
 import java.util.Scanner;
 
 public class Main {
