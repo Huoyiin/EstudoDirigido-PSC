072ed3292ec86d2334ec94f375d3eaa9cfed351d
Emily Carolina <128648356+Huoyiin@users.noreply.github.com>
2023-05-03 21:28:32 -0300

    Update Questao10.java

--- Questao10.java
+++ Questao10.java
@@ -1,3 +1,9 @@
+//Faça uma classe com um método que será usado em todo sistema que irá verificar se
+//um valor é perfeito ou não. Um valor é dito perfeito quando ele é igual a soma dos
+//seus divisores excetuando ele próprio. (Ex: 6 é perfeito, 6 = 1 + 2 + 3, que são seus
+//divisores). O método deve retornar um valor booleano.
+
+
 import java.util.Scanner;
 
 public class Main {
