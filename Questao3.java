b802c9955dd414b75a8479c7ed2eba5afe128453
Emily Carolina <128648356+Huoyiin@users.noreply.github.com>
2023-05-03 21:25:37 -0300

    Questao3.java

--- Questao3.java
+++ Questao3.java
@@ -1,3 +1,7 @@
+//Ler dois valores para as variáveis A e B, e efetuar as trocas dos valores de forma que a
+//variável A passe a possuir o valor da variável B e a variável B passe a possuir o valor da
+//variável A. Apresentar os valores das variáveis iniciais e depois os valores trocados.
+
 import java.util.Scanner;
 
 public class Main {
